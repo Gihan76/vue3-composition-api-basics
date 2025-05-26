538e22023bf419075a6e375fb4af9080fac451cb
Gihan Wijesinghe <gihantharaka76@gmail.com>
2025-05-26 08:20:24 +0530

    accessed the params in the script tag of the template using useRoute()

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -2,6 +2,9 @@
   <div class="post-detail">
     <h1>This is a post page</h1>
     <p>Display the content of post with ID of {{ $route.params.id }} here</p>
+    <div>
+      <button @click="showPostId">Show Post ID</button>
+    </div>
     <p>
       <RouterLink to="/posts">&lt; Back</RouterLink>
     </p>
@@ -9,5 +12,11 @@
 </template>
 
 <script setup>
+  import { useRoute } from 'vue-router';
 
+  const route = useRoute();
+
+  const showPostId = () => {
+    alert(`Post ID: ${route.params.id}`);
+  }
 </script>
