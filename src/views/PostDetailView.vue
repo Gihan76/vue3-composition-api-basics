5a2c34ebf609d466072a91bea509a65d178eabcb
Gihan Wijesinghe <gihantharaka76@gmail.com>
2025-05-26 08:31:19 +0530

    navigate to different routes using useRouter()

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -5,6 +5,12 @@
     <div>
       <button @click="showPostId">Show Post ID</button>
     </div>
+    <div>
+      <button @click="goHomeIn3Seconds">Go Home in 3 seconds</button>
+    </div>
+    <div>
+      <button @click="goToFirstPost">Go to First Post</button>
+    </div>
     <p>
       <RouterLink to="/posts">&lt; Back</RouterLink>
     </p>
@@ -12,11 +18,29 @@
 </template>
 
 <script setup>
-  import { useRoute } from 'vue-router';
+  import { useRoute, useRouter } from 'vue-router';
 
   const route = useRoute();
+  const router = useRouter();
 
   const showPostId = () => {
     alert(`Post ID: ${route.params.id}`);
   }
+
+  const goHomeIn3Seconds = () => {
+    setTimeout(() => {
+      // router.push('/'); // by path of the router
+      // or 
+      router.push({ name: 'home'}) // by name of the router
+    }, 3000);
+  };
+
+  const goToFirstPost = () => {
+    // -> postDetail/id1
+    router.push({
+      name: 'postDetail',
+      params: { id: 'id1'},
+    })
+  };
+  
 </script>
