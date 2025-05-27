242df6e4636f1062f3f11bc54615d76550a1af11
Gihan Wijesinghe <gihantharaka76@gmail.com>
2025-05-27 07:00:07 +0530

    teleport implemented

---
+++ src/views/ModalsView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="modals">
+        <h1>Modals</h1>
+        <button @click="showModal = true">Show modal</button>
+        <teleport to="modals-container">
+            <div v-if="showModal" class="modal">
+                <h1>This is a modal</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita maiores vel facere laborum, quia
+                    quibusdam. Voluptatibus, eaque! Pariatur, esse neque nulla suscipit, reprehenderit cupiditate nisi
+                    unde
+                    ad dolore expedita magnam!
+                </p>
+                <button @click="showModal = false">Hide modal</button>
+            </div>
+        </teleport>
+
+    </div>
+</template>
+
+<script setup>
+import { ref, Teleport } from 'vue';
+
+const showModal = ref(false);
+</script>
+
+<style>
+.modal {
+    background: red;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+</style>
