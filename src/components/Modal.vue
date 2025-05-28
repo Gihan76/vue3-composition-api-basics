4bc0ce8375339d4d50aa0761b2dda6e7c3b81ccd
Gihan Wijesinghe <gihantharaka76@gmail.com>
2025-05-28 06:21:36 +0530

    child component options vs composition api

---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+    <teleport to=".modals-container">
+        <div class="modal">
+            <h1>This is a modal</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita maiores vel facere laborum, quia
+                quibusdam. Voluptatibus, eaque! Pariatur, esse neque nulla suscipit, reprehenderit cupiditate nisi
+                unde
+                ad dolore expedita magnam!
+            </p>
+            <button>Hide modal</button>
+        </div>
+    </teleport>
+</template>
+
+<style>
+.modal {
+    background: red;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,37 +2,26 @@
     <div class="modals">
         <h1>Modals</h1>
         <button @click="showModal = true">Show modal</button>
-        <teleport to="modals-container">
-            <div v-if="showModal" class="modal">
-                <h1>This is a modal</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita maiores vel facere laborum, quia
-                    quibusdam. Voluptatibus, eaque! Pariatur, esse neque nulla suscipit, reprehenderit cupiditate nisi
-                    unde
-                    ad dolore expedita magnam!
-                </p>
-                <button @click="showModal = false">Hide modal</button>
-            </div>
-        </teleport>
+        <Modal v-if="showModal"></Modal>
 
     </div>
 </template>
 
+<!-- composition api -->
 <script setup>
-import { ref, Teleport } from 'vue';
+import { ref } from 'vue';
+import Modal from '@/components/Modal.vue';
 
 const showModal = ref(false);
 </script>
 
-<style>
-.modal {
-    background: red;
-    padding: 10px;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-}
-</style>
+<!-- options api -->
+<!-- <script>
+    import Modal from '@/components/Modal.vue';
+
+    export default {
+        components: {
+            Modal
+        }
+    }
+</script> -->
